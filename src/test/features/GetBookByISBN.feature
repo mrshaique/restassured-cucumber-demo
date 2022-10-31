149f498bc710368f67f07c6ebc32617fda2f363e
sabiden99@gmail.com <sabiden99@gmail.com>
2022-10-30 21:40:25 -0400

    -Added 3rd API to test, a BitCoin Price Checker -Create 2 tests to get price from API, static with USD, and dynamic with input currency

--- src/test/features/GetBookByISBN.feature
+++ src/test/features/GetBookByISBN.feature
@@ -1,5 +1,18 @@
-Feature: Get book by ISBN
+Feature: Testing Asynchronous APIs
 
+  @GeneralAPIValidationTest
+  Scenario: The User has APIs they would like to validate
+    Given the API exists
+    When A user gets info from the API 1
+    Then the status code is 200
+    Given the API exists
+    When A user gets info from the API 2
+    Then the status code is 200
+    When A user gets info from the API 3
+    Then the status code is 200
+
+
+  @GoogleBooksAPIValidation
   Scenario: User calls web service to get a book by its ISBN
     Given the user can query by isbn 9781451648546
     When A user gets info from the API 1
@@ -15,8 +28,7 @@ Feature: Get book by ISBN
       | items.volumeInfo.categories[0] | Biography & Autobiography |
       | items.volumeInfo.averageRating | 4                         |
 
-
-  @SampleAPITest
+  @RandomFactAPITest
   Scenario: User calls gets information from an API 2
     Given the API exists
     When A user gets info from the API 2
@@ -25,6 +37,24 @@ Feature: Get book by ISBN
       | length | 2             |
     And response includes values below these thresholds
       | length | 500             |
+
+  @BitCoinAPIValidation
+  Scenario: The User has BitCoin API USD price they want to check
+    Given the API exists
+    When A user gets info from the API 3
+    Then the status code is 200
+    And the user checks the price
+
+  @BitCoinAPIValidationDynamic
+  Scenario: The User has BitCoin API price they want to check
+    Given the API exists
+    When A user gets info from the API 3
+    Then the status code is 200
+    And the price is checked in the currency USD
+    And the price is checked in the currency GBP
+    And the price is checked in the currency EUR
+
+
 
 
 
--- src/test/java/stepdefs/BookStepDefinitions.java
+++ src/test/java/stepdefs/BookStepDefinitions.java
@@ -3,8 +3,10 @@ package stepdefs;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import io.restassured.path.json.JsonPath;
 import org.apache.commons.lang3.StringUtils;
 
 import cucumber.api.java.en.And;
@@ -23,24 +25,71 @@ public class BookStepDefinitions {
 
 	private String BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 	private String BASE_URL2 = "https://catfact.ninja/fact";
-	private String[] URL_Arr = new String[] {"","https://www.googleapis.com/books/v1/volumes","https://catfact.ninja/fact"};
-
-
+	private String[] URL_Arr = new String[] {"","https://www.googleapis.com/books/v1/volumes","https://catfact.ninja/fact", "https://api.coindesk.com/v1/bpi/currentprice.json"};
 
 	@Given("the user can query by isbn (.*)")
 	public void a_book_exists_with_isbn(String isbn){
 		request = given().param("q", "isbn:" + isbn);
 	}
 
-//	@When("a user retrieves the book by isbn")
-//	public void a_user_retrieves_the_book_by_isbn(){
-//		response = request.when().get(BASE_URL);
-//		System.out.println("response: " + response.prettyPrint());
-//	}
+	@Given("a book exists with an title of (.*)")
+	public void a_book_exists_with_title(String title){
+		request = given().param("q", ""  + title);
+		System.out.println(request);
+	}
+
+	@Given("the API exists")
+	public void api_exists() {
+		request = given().param("q", "x");
+	}
+
+	@When("a user retrieves the book by Title")
+	public void a_user_retrieves_the_book_by_Title(){
+		response = request.when().get(BASE_URL);
+		System.out.println("response: " + response.prettyPrint());
+	}
+
+	@When("A user gets info from the API (\\d+)")
+	public void a_user_gets_response_from_API(int apiNum){
+		response = request.when().get(URL_Arr[apiNum]);
+		//System.out.println("response: " + response.prettyPrint());
+	}
 
 	@Then("the status code is (\\d+)")
 	public void verify_status_code(int statusCode){
 		json = response.then().statusCode(statusCode);
+	}
+
+	@And("response includes values above these thresholds$")
+	public void response_above(Map<String,String> responseFields){
+		for (Map.Entry<String, String> field : responseFields.entrySet()) {
+			json.body(field.getKey(), greaterThanOrEqualTo(Integer.parseInt(field.getValue())));
+			}
+		}
+
+	@And("response includes values below these thresholds$")
+	public void response_below(Map<String,String> responseFields){
+		for (Map.Entry<String, String> field : responseFields.entrySet()) {
+			json.body(field.getKey(), lessThanOrEqualTo(Integer.parseInt(field.getValue())));
+		}
+	}
+
+	@And("the price is checked in the currency (.*)")
+		public void query_price_dynam(String region){
+		JsonPath responsePath = response.jsonPath();
+		LinkedHashMap<String, LinkedHashMap<String, String>> bpiPath = responsePath.get("bpi");
+		LinkedHashMap<String, String> USPath = bpiPath.get(region);
+		String price = USPath.get("rate");
+		System.out.println("Price received from Response is: " + price + " " + region);
+	}
+
+	@And("the user checks the price")
+	public void query_price(){
+		JsonPath responsePath = response.jsonPath();
+		LinkedHashMap<String, LinkedHashMap<String, String>> bpiPath = responsePath.get("bpi");
+		LinkedHashMap<String, String> USPath = bpiPath.get("USD");
+		String price = USPath.get("rate");
+		System.out.println("Price received from Response $" + price );
 	}
 
 	@And("response includes the following$")
@@ -67,44 +116,6 @@ public class BookStepDefinitions {
 		}
 	}
 
-	@Given("a book exists with an title of (.*)")
-	public void a_book_exists_with_title(String title){
-		request = given().param("q", ""  + title);
-		System.out.println(request);
-	}
-
-	@When("a user retrieves the book by Title")
-	public void a_user_retrieves_the_book_by_Title(){
-		response = request.when().get(BASE_URL);
-		System.out.println("response: " + response.prettyPrint());
-	}
-
-	@Given("the API exists")
-	public void api_exists(){
-		request = given().param("q");
-	}
-
-
-
-	@And("response includes values above these thresholds$")
-	public void response_above(Map<String,String> responseFields){
-		for (Map.Entry<String, String> field : responseFields.entrySet()) {
-			json.body(field.getKey(), greaterThanOrEqualTo(Integer.parseInt(field.getValue())));
-			}
-		}
-
-	@And("response includes values below these thresholds$")
-	public void response_below(Map<String,String> responseFields){
-		for (Map.Entry<String, String> field : responseFields.entrySet()) {
-			json.body(field.getKey(), lessThanOrEqualTo(Integer.parseInt(field.getValue())));
-		}
-	}
-
-	@When("A user gets info from the API (\\d+)")
-        public void a_user_gets_response_from_API(int apiNum){
-			response = request.when().get(URL_Arr[apiNum]);
-			System.out.println("response: " + response.prettyPrint());
-	}
 }
 
 
